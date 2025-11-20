9a6da353efcc6ef3fbdff69a5449815121bc2d76
GavinMero <158069670+GavinMero@users.noreply.github.com>
2025-11-20 02:57:50 +0000

    Testing commit from work lapotp to personal, using Home.jsx.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Guitar Academy</h1>
-            <p>Your journey starts here. Let's start!</p>
+            <p>Your journey starts here. Let's start learning!</p>
         </div>
     );
 }
