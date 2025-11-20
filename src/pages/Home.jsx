0993a36975f122bef605e0d1e4a4e05c5c275567
gavin <gavinmero@gmail.com>
2025-11-19 21:48:07 -0500

    Testing commits again, using Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Guitar Academy</h1>
-            <p>Your journey starts here. Let's begin!</p>
+            <p>Your journey starts here. Let's start!</p>
         </div>
     );
 }
