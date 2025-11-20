954f4018f360ecaffa5c0118033aa54e04fc2c68
gavin <gavinmero@gmail.com>
2025-11-19 21:24:02 -0500

    Testing how to stage using Home.jsx.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Guitar Academy</h1>
-            <p>Your journey starts here.</p>
+            <p>Your journey starts here. Let's begin!</p>
         </div>
     );
 }
